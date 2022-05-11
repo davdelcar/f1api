c558970c5484be5b27f7cb8ea491ad9ac4f44376
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2022-05-11 13:24:49 +0200

    Race

--- src/main/java/com/aiss/f1api/repositories/RaceRepository.java
+++ src/main/java/com/aiss/f1api/repositories/RaceRepository.java
@@ -1,8 +1,11 @@
 package com.aiss.f1api.repositories;
 
+import com.aiss.f1api.models.RaceModel;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RaceRepository {
+public interface RaceRepository extends CrudRepository<RaceModel, Long>{
     
 }
