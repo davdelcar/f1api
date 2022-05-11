1219ec53ed4835ad809e4c97f649c0e4eaf06f1d
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2022-05-11 13:59:07 +0200

    cambios

--- src/main/java/com/aiss/f1api/controllers/ConstructorController.java
+++ src/main/java/com/aiss/f1api/controllers/ConstructorController.java
@@ -1,11 +1,17 @@
 package com.aiss.f1api.controllers;
 
+import com.aiss.f1api.services.ConstructorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/controller")
+@RequestMapping("/constructor")
 public class ConstructorController {
+    @Autowired
+    ConstructorService constructorService;
+
 
     //@GetMapping()
 
--- src/main/java/com/aiss/f1api/controllers/RaceController.java
+++ src/main/java/com/aiss/f1api/controllers/RaceController.java
@@ -1,11 +1,16 @@
 package com.aiss.f1api.controllers;
 
+import com.aiss.f1api.services.RaceService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/race")
 public class RaceController {
+    @Autowired
+    RaceService raceService;
 
     //@GetMapping()
 
