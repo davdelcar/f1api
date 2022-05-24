755c71af777279263d2cf2987dc48fe59626e515
David del Hoyo Carcaboso <davdelcar@alum.us.es>
2022-05-24 18:45:20 +0200

    /swagger-ui/index.html

--- src/main/java/com/aiss/f1api/Swagger/swaggerConfig.java
+++ src/main/java/com/aiss/f1api/Swagger/swaggerConfig.java
@@ -1,31 +1,36 @@
 package com.aiss.f1api.swagger;
-import java.util.Collections;
-
-import org.hibernate.annotations.Any;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.RequestHandledEvent;
-
-import io.swagger.models.Path;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
 
-	@Bean
-	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.aiss.f1api.services"))
-				.paths(PathSelectors.any())
-				.build()
-				;
-	}
-	
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.aiss.f1api"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiInfoMetaData());
+    }
+
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("NAME OF SERVICE")
+                .description("API Endpoint Decoration")
+                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
